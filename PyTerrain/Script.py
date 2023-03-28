e18bd1663511a14c053ef3c2bf56297cf40972d1
Shaunak Ghosh <128846645+shaunakg3@users.noreply.github.com>
2023-03-28 18:58:52 +0100

    Update Script.py

--- PyTerrain/Script.py
+++ PyTerrain/Script.py
@@ -6,8 +6,9 @@ from mpl_toolkits import mplot3d
 ax = plt.axes(projection="3d")
 x_data = np.arange(-5, 5, 0.1)
 y_data = np.arange(-5, 5, 0.1)
-X, Y = np.meshgrid(x_data, y_data)
-Z = np.sin(3.14 * X) * np.cos(3.14 * Y)
+X, Y = np.meshgrid(x_data, y_data) # Compute meshes
+Z = np.sin(X) * np.cos(Y)
 
+# Plot mesh
 ax.plot_surface(X, Y, Z, cmap="viridis")
 plt.show()
